a79097c205604fa322b1f47dd268abf336c3103a
ianhi <ianhuntisaak@gmail.com>
2019-04-19 13:08:25 -0400

    more utils

--- glrm/util.py
+++ glrm/util.py
@@ -45,3 +45,74 @@ def shrinkage(a, kappa):
     """ soft threshold with parameter kappa). """
     try: return maximum(a - kappa(ones(a.shape), 0)) - maximum(-a - kappa*ones(a.shape), 0)
     except: return max(a - kappa, 0) - max(-1 - kappa, 0)
+def missing2mask(A_shape,missing_idx):
+    """
+    generates a mask from missing_idx
+    
+    input
+    -----
+    A_shape : shape
+        Shape of the data matrix
+    missing_idx : array
+        shape (n_missing, 2) with first column the row and 2nd column the column of the missing index
+        
+    returns
+    =------
+    
+    a mask with False where values are missing
+    """
+    
+    mask = np.ones(A_shape,dtype=np.bool)
+    mask[missing_idx[:,0],missing_idx[:,1]]=False
+    return mask
+
+def gen_random_missing_mask(A,n_missing,return_indices = False):
+    """
+    generates a mask + (optional) indices of the missing data
+    """
+    missing_idx =np.random.choice(np.arange(A.shape[0]),size=(n_missing,1),replace=False)
+    missing_idx =np.hstack([missing_idx,np.random.randint(0,A.shape[1],size=(n_missing,1))])
+    
+    mask = np.ones_like(A,dtype=np.bool)
+    mask[missing_idx[:,0],missing_idx[:,1]]=False
+    if return_indices:
+        return mask, missing_idx
+    else:
+        return mask
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+def oneHot(A,columns,max_index = None,):
+    """
+    does one hot encoding of the categorical values
+    
+    Assumes that the categoricals are already integers starting from 0
+    A : data matrix
+    columns : iterable of the columns that contain categoricals
+    max_index: max value of classes , if None will be inferred. If not None
+        then needs to be the same shape as oclumns
+        
+    could probably also call the sklearn onehotencoder
+    """
+    A_col = A[:,columns]
+    if max_index is None:
+        max_index = A_col.max(axis=0)
+        print(max_index)
+    elif len(max_index) != len(columns):
+        raise ValueError('Yikes len(max_index) != len(columns)')
+    else:
+        max_index = np.asarray(max_index)
+    m = A_col.shape[0]
+    one_hot = []
+    for i in range(len(columns)):
+        one_hot.append(np.zeros(m,max_index[i]+1))
+        one_hot[-1] = np.eye(max_index[i]+1)[A_col[:,i]]
+    return one_hot
+
+def oneHotTransform(A,columns,max_index = None):
+    """
+    does one hot encoding of the categorical values and transforms the array
+    """
+    one_hot = oneHot(A,columns,max_index)
+    #stack the onehot stuff 
+    return np.hstack([A[:,i][:,None] if not i in columns else one_hot[np.abs(columns-i).argmax()] for i in range(A.shape[1])])
